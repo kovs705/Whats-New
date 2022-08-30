3be9c35aea7154bc50dd8e49c9deaa3d4deda70d
kovs705 <smartkovsik@gmail.com>
2022-08-31 01:41:08 +0300

    added some instructions for future developing. Now I can go to sleep

---
+++ Whats New/LandingPage.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Whats New
+//
+//  Created by Kovs on 31.08.2022.
+//
+
+import SwiftUI
+
+// okay, it's been a while since I wrote in SwiftUI, so it can be tough when I'll work
+// with this app and page, but let's make some starting points;
+
+// 1 - Landing page with a button to open a modal VC (random color send with a push)
+// 2 - modal VC should has a VStack with a button
+// 3 - Add a Json file to work with base words of "Whats new"
+// 4 - A Data file to work with Json
+// 5 - TextFields with an ability to change some texts in the app?
+
+struct LandingPage: View {
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPage()
+    }
+}
