1e92bfadd90ebc2437238b42b1f71df4ff1ecc50
kovs705 <smartkovsik@gmail.com>
2022-08-31 01:33:45 +0300

    Initial Commit

---
+++ Whats New/Whats_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whats_NewApp.swift
+//  Whats New
+//
+//  Created by Kovs on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Whats_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
