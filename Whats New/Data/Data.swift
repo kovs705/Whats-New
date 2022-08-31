c004a606f84a1addd7cee9c233b30c94d97dab48
kovs705 <smartkovsik@gmail.com>
2022-08-31 13:57:42 +0300

    added struct and json file

---
+++ Whats New/Data/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  Whats New
+//
+//  Created by Kovs on 31.08.2022.
+//
+
+import SwiftUI
+
+struct Features: Codable, Equatable, Identifiable {
+    // place properties of json features..
+    
+    
+}
+
